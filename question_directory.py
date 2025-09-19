08bbb5d0d3e486f254194961891204d613f22f0f
johdy <jgmdyer@gmail.com>
2025-09-19 13:00:53 +0200

    rename

--- question_directory.py
+++ question_directory.py
@@ -4,7 +4,7 @@ import pprint
 
 from PIL import Image
 
-from img_q_and_a import img_q_and_a
+from image_q_and_a import image_q_and_a
 
 def question_directory(directory: List[str], questions: List[str]) -> Dict:
     results = []
@@ -12,7 +12,7 @@ def question_directory(directory: List[str], questions: List[str]) -> Dict:
         image = Image.open(file).convert("RGB")
         q_a = []
         for q in questions:
-            answer = img_q_and_a(image, q)
+            answer = image_q_and_a(image, q)
             q_a.append({q: answer})
         results.append({"filename": file, "Q&A": q_a})
         pprint.pp(results)
